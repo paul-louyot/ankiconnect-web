0924813cc1e2a1c5733ad51b9c83622553549c1d
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2026-07-03 14:51:20 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>welcome</>;
+  return (
+    <>
+      <div className="min-h-dvh flex flex-col justify-center items-center">
+        <form className="flex flex-col gap-4">
+          <input type="text" name="front" className="input" />
+          <input type="text" name="back" className="input" />
+          <select name="deckName" className="select">
+            <option>All::Dev</option>
+            <option>All::Gen</option>
+            <option>All::Quotes</option>
+            <option>All::Lang::English</option>
+          </select>
+          <button type="submit" className="btn">
+            submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
